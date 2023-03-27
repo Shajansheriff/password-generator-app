64b9f7255760e54f25caea3d530b19eea624e18e
Shajahan A <Shajansheriff@users.noreply.github.com>
2023-03-27 14:32:25 +0530

    calculate password strength

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/Button";
 import { Checkbox } from "@/components/Checkbox";
 import { Label } from "@/components/Label";
 import { Slider } from "@/components/Slider";
+import { StrengthBar } from "@/components/StengthBar";
 import { FormEventHandler, useEffect, useState } from "react";
 
 const alphabets_lowercase = "abcdefghijklmnopqrstuvwxyz";
@@ -17,10 +18,10 @@ function randomInteger(min: number, max: number) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const getRandomType = (types: Type[]) => {
-  const index = randomInteger(0, types.length);
+const getRandomType = (types: Set<Type>) => {
+  const index = randomInteger(0, types.size);
 
-  const result = types[index];
+  const result = [...types][index];
   return result;
 };
 
@@ -30,7 +31,7 @@ const getRandomChar = (str: string) => {
 };
 interface Config {
   length: number;
-  rules: Type[];
+  rules: Set<Type>;
 }
 
 const generate = ({ length, rules }: Config) => {
@@ -56,20 +57,69 @@ const generate = ({ length, rules }: Config) => {
   return string;
 };
 
+const POOR = 1;
+const WEAK = 2;
+const MEDIUM = 3;
+const STRONG = 4;
+
+const calculatePasswordStrength = ({
+  length,
+  rules,
+}: Config): 1 | 2 | 3 | 4 => {
+  if (length < 5 || rules.size < 2) {
+    return POOR;
+  }
+
+  if (length > 4 && length < 8) {
+    switch (rules.size) {
+      case 0:
+      case 1:
+        return POOR;
+      case 2:
+        return WEAK;
+      default:
+      case 3:
+        return MEDIUM;
+    }
+  }
+
+  switch (rules.size) {
+    case 0:
+    case 1:
+      return POOR;
+    case 2:
+      return WEAK;
+    case 3:
+      return MEDIUM;
+    default:
+    case 4:
+      return STRONG;
+  }
+};
+
 export default function Home() {
   const [length, setLength] = useState(8);
   const [password, setPassword] = useState("");
+  const [strength, setStrength] = useState(4);
   useEffect(() => {
     if (!password) {
-      setPassword(generate({ length, rules: ["lowercase"] }));
+      setPassword(
+        generate({
+          length,
+          rules: new Set(["lowercase", "uppercase", "numbers", "symbols"]),
+        })
+      );
     }
   }, [length, password]);
   const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
-    const rules = (formData.getAll("rules") ?? []) as Type[];
+    const rulesFormValues = (formData.getAll("rules") ?? []) as Type[];
+    const rules = new Set(rulesFormValues);
+    setStrength(calculatePasswordStrength({ length, rules }));
     setPassword(generate({ length, rules }));
   };
+
   return (
     <main>
       {password}
@@ -85,7 +135,7 @@ export default function Home() {
           onChange={(e) => setLength(parseInt(e.target.value, 10))}
         />
         <Label>
-          <Checkbox name="rules" value={"uppercase"} />
+          <Checkbox name="rules" value={"uppercase"} defaultChecked={true} />
           Include Uppercase Letters
         </Label>
         <Label>
@@ -98,13 +148,14 @@ export default function Home() {
           Include Lowercase Letters
         </Label>
         <Label>
-          <Checkbox name="rules" value="numbers" />
+          <Checkbox name="rules" value="numbers" defaultChecked={true} />
           Include Numbers
         </Label>
         <Label>
-          <Checkbox name="rules" value="symbols" />
+          <Checkbox name="rules" value="symbols" defaultChecked={true} />
           Include Symbols
         </Label>
+        <StrengthBar strength={strength} />
         <Button type="submit">Generate</Button>
       </form>
     </main>
